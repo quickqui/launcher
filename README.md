f202ae4ff1db29062fee125a4dc9a499a3b87a80
nielinjie <nielinjie@gmail.com>
2020-05-14 16:24:17 +0800

    debug

--- README.md
+++ README.md
@@ -1,16 +1,20 @@
-# quick-qui/launcher
+# @quick-qui/launcher
 
-实现的一部分，负责将各个[implementation]()的运行时运行起来。
-
-模型相关概念请参照 - （TODO）
+实现的一部分，负责将运行时运行起来。
 
 
+## 如何工作
 
-## 如何工作 -
+1. [@quick-qui/builder](http://github.com/quickqui/builder) 会负责准备好运行时需要的所有材料。体现为一个目录，称为“成品目录”。
+2. 根据成品目录中的配置（./.env 和./implementationModel.json)运行一个[@quick-qui/model-server](https://github.com/quickqui/model-server) 实例。
+3. 从 model-server 得到模型。
+4. 根据模型（主要是其中的 implementations 部分）启动各个实现。
+5. 目前支持的运行时类型是 -
+   1. command - 本地可执行命令运行。
+   2. docker - docker container 运行。
+   3. 🏃🏻npm - 类似于command，只不过不是直接的路径配置，而是通过npm的依赖机制寻找实现的运行路径。
 
-1. 自己运行起来。
-2. 根据配置找到model-server，从model-server得到模型。
-3. 根据模型（主要是其中的implementations部分）启动各个implementation。
-4. 目前支持的运行时类型是 -
-    1. command - 本地可执行命令运行。
-    2. docker - docker container运行。 （TODO）
+
+## 如何运行
+
+作为npm依赖，被引入到“成品目录”中，被成品目录中的脚本（一般是node脚本）调用。不会直接运行。
--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,35 @@
 export { dockerLaunch, modelServerConfig } from "./DockerLauncher";
-export { rawLaunch} from "./RawLauncher"
+export { rawLaunch } from "./RawLauncher";
 export { filterObject, noEnvFound } from "./Util";
 import assert, { fail } from "assert";
 import { ImplementationModel } from "@quick-qui/model-defines";
 import { dockerLaunch } from "./DockerLauncher";
 import { rawLaunch } from "./RawLauncher";
 
-export function launch(implementationModel:ImplementationModel){
-    const launcherType = process.env["LAUNCHER_TYPE"];
-    const launcherName = process.env["LAUNCHER_NAME"];
+export function launch(implementationModel: ImplementationModel) {
+  const launcherType = process.env["LAUNCHER_TYPE"];
+  const launcherName = process.env["LAUNCHER_NAME"];
 
+  const launcherImplementation = implementationModel?.implementations.find(
+    (implementation) =>
+      implementation.abstract !== true &&
+      implementation.runtime === "launcher" &&
+      ((launcherName ? implementation.name === launcherName : false) ||
+        launcherType === implementation.parameters?.type)
+  );
 
-    const launcherImplementation = implementationModel?.implementations.find(
-      (implementation) =>
-        implementation.runtime === "launcher" &&
-        ((launcherName ? implementation.name === launcherName : false) ||
-          launcherType === implementation.parameters?.type)
-    );
-
-    if (launcherImplementation) {
-      if (launcherType === "docker") {
-        dockerLaunch(launcherImplementation, implementationModel);
-      } else if (launcherType === "raw") {
-        rawLaunch(launcherImplementation, implementationModel);
-      } else if (launcherType === "node") {
-        //TODO
-        fail(`launcher type not supported yet - ${launcherType}`);
-      } else {
-        fail(`launcher type not supported yet - ${launcherType}`);
-      }
+  if (launcherImplementation) {
+    if (launcherType === "docker") {
+      dockerLaunch(launcherImplementation, implementationModel);
+    } else if (launcherType === "raw") {
+      rawLaunch(launcherImplementation, implementationModel);
+    } else if (launcherType === "node") {
+      //TODO
+      fail(`launcher type not supported yet - ${launcherType}`);
     } else {
-      fail(`no launcher found - name=${launcherName}`);
+      fail(`launcher type not supported yet - ${launcherType}`);
     }
-
+  } else {
+    fail(`no launcher found - name=${launcherName}`);
+  }
 }
-
